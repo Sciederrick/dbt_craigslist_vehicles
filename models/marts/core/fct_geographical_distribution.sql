331ed9f4f4ca4a8909d4bf2dd21f895b63561a2d
Sciederrick <derrickmbarani@gmail.com>
2023-12-16 12:48:45 +0300

    fix: fct_geographical_distribution model

--- models/marts/core/fct_geographical_distribution.sql
+++ models/marts/core/fct_geographical_distribution.sql
@@ -1,6 +1,6 @@
 -- Inventory: Geographical distribution of vehicle listing
 
-select d.state, d.region, count(*)
+select d.state, d.region, count(*) as vehicle_count
 from {{ ref('stg_car_sales') }} as s join {{ ref('stg_car_details') }} as d using(id)
 group by d.state, d.region
-order by d.state, d.region
+order by d.state, d.region, vehicle_count DESC
